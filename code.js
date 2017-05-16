e40acec0223655bb1a0fcd886cb06d3e6864c207
Alexander Kochergin <ejiqpep@gmail.com>
2017-05-16 21:40:30 +0200

    08 form validation

--- code.js
+++ code.js
@@ -1,8 +1,52 @@
-var app = document.querySelector('.app')
-var title = document.createElement('div')
-title.className = 'title'
-title.innerHTML = 'This is the title of news'
+var form = document.querySelector('.formWithValidation')
+var validateBtn = form.querySelector('.validateBtn')
+var from = form.querySelector('.from')
+var password = form.querySelector('.password')
+var passwordConfirmation = form.querySelector('.passwordConfirmation')
+var where = form.querySelector('.where')
+var message = form.querySelector('.message')
+var fields = form.querySelectorAll('.field')
 
-app.insertBefore(title, app.firstChild)
+var generateError = function (text) {
+  var error = document.createElement('div')
+  error.className = 'error'
+  error.style.color = 'red'
+  error.innerHTML = text
+  return error
+}
 
-title.remove()
+var removeValidation = function () {
+  var errors = form.querySelectorAll('.error')
+
+  for (var i = 0; i < errors.length; i++) {
+    errors[i].remove()
+  }
+}
+
+var checkFieldsPresence = function () {
+  for (var i = 0; i < fields.length; i++) {
+    if (!fields[i].value) {
+      console.log('field is blank', fields[i])
+      var error = generateError('Cannot be blank')
+      form[i].parentElement.insertBefore(error, fields[i])
+    }
+  }
+}
+
+var checkPasswordMatch = function () {
+  if (password.value !== passwordConfirmation.value) {
+    console.log('not equals')
+    var error = generateError('Password doesnt match')
+    password.parentElement.insertBefore(error, password)
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  removeValidation()
+
+  checkFieldsPresence()
+
+  checkPasswordMatch()
+})
